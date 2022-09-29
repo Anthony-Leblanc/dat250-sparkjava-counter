b184c72898ecabb3823245ccfae59cb49b89ccdd
Anthony-Leblanc <anthony.leblanc@edu.univ-paris13.fr>
2022-09-29 16:11:57 +0200

    Update, should works

--- todos/src/main/java/no/hvl/dat250/rest/todos/Todo.java
+++ todos/src/main/java/no/hvl/dat250/rest/todos/Todo.java
@@ -1,49 +1,61 @@
 package no.hvl.dat250.rest.todos;
 
+import com.google.gson.Gson;
+
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class Todo {
-    private final Long id;
-    private final String summary;
-    private final String description;
+  private static final AtomicLong count = new AtomicLong(0);
+  private final Long id;
+  private String summary;
+  private String description;
 
-    public Todo(Long id, String summary, String description) {
-        this.id = id;
-        this.summary = summary;
-        this.description = description;
-    }
+  public Todo(Long id, String summary, String description) {
+    this.id = id;
+    this.summary = summary;
+    this.description = description;
+  }
 
-    public Todo(String summary, String description) {
-        this(null, summary, description);
-    }
+  public Todo(String summary, String description) {
+    this(count.incrementAndGet(), summary, description);
+  }
 
-    /**
-     * ID might be null!
-     */
-    public Long getId() {
-        return id;
-    }
+  /**
+   * ID might be null!
+   */
+  public Long getId() {
+    return id;
+  }
 
-    public String getSummary() {
-        return summary;
-    }
+  public String getSummary() {
+    return summary;
+  }
 
-    public String getDescription() {
-        return description;
-    }
+  public void setSummary(String summary){this.summary = summary; }
 
-    // Do not change equals and hashcode!
+  public String getDescription() { return description; }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Todo todo = (Todo) o;
-        return Objects.equals(id, todo.id) && Objects.equals(summary, todo.summary) && Objects.equals(description, todo.description);
-    }
+  public void setDescription(String description){this.description = description; }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, summary, description);
-    }
+  public String toJson()
+  {
+    Gson gson = new Gson();
+    String jsonInString = gson.toJson(this);
+    return jsonInString;
+  }
+  // Do not change equals and hashcode!
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Todo todo = (Todo) o;
+    return Objects.equals(id, todo.id) && Objects.equals(summary, todo.summary) && Objects.equals(description, todo.description);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, summary, description);
+  }
 }
--- todos/src/main/java/no/hvl/dat250/rest/todos/TodoAPI.java
+++ todos/src/main/java/no/hvl/dat250/rest/todos/TodoAPI.java
@@ -1,21 +1,101 @@
 package no.hvl.dat250.rest.todos;
 
+import com.google.gson.Gson;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.regex.Pattern;
+
 import static spark.Spark.*;
 
 /**
  * Rest-Endpoint.
  */
 public class TodoAPI {
-
-    public static void main(String[] args) {
-        if (args.length > 0) {
-            port(Integer.parseInt(args[0]));
-        } else {
-            port(8080);
-        }
-
-        after((req, res) -> res.type("application/json"));
-
-        // TODO: Implement API, such that the testcases succeed.
+  public static void main(String[] args) {
+    if (args.length > 0) {
+      port(Integer.parseInt(args[0]));
+    } else {
+      port(6000);
     }
+    after((req, res) -> res.type("application/json"));
+
+    // TODO: Implement API, such that the testcases succeed.
+    Set<Todo> todos = new HashSet<>();
+
+    get(
+            "/todos",
+            (req,res)->
+            {
+              Gson gson = new Gson();
+              return gson.toJson(todos);
+            }
+    );
+    get(
+            "/todos/:id",
+            (req,res)->
+            {
+              Gson gson = new Gson();
+              if(!req.params(":id").matches("-?\\d+(\\.\\d+)?")){
+                return String.format("The id \"%s\" is not a number!", req.params(":id"));}
+              for (Todo todo : todos){
+                if ( req.params(":id").equals(todo.getId().toString())) {
+                  return todo.toJson();
+                }}
+              return String.format("Todo with the id \"%s\" not found!", req.params(":id"));
+
+            }
+    );
+    put(
+            "/todos/:id",
+            (req,res)->
+            {
+              Gson gson = new Gson();
+              if(!req.params(":id").matches("-?\\d+(\\.\\d+)?")){
+                return String.format("The id \"%s\" is not a number!", req.params(":id"));}
+              Todo inputTodo = gson.fromJson(req.body(),Todo.class);
+              for (Todo todo : todos){
+                if ( req.params(":id").equals(todo.getId().toString())) {
+                  todo.setSummary(inputTodo.getSummary());
+                  todo.setDescription(inputTodo.getDescription());
+                  return "Updated";
+                }}
+
+              return String.format("Todo with the id \"%s\" not found!", req.params(":id"));
+            }
+    );
+    post(
+            "/todos",
+            (req,res)->
+            {
+              Todo todo = new Todo(null,null);
+              Gson gson = new Gson();
+              todo = gson.fromJson(req.body(),Todo.class);
+
+              todos.add(todo);
+              return todo.toJson();
+            }
+
+    );
+
+    delete(
+            "/todos/:id",
+            (req,res)->
+            {
+              if(!req.params(":id").matches("-?\\d+(\\.\\d+)?")){
+                return String.format("The id \"%s\" is not a number!", req.params(":id"));}
+              Gson gson = new Gson();
+              for (Todo todo : todos) {
+                if (req.params(":id").equals(todo.getId().toString())) {
+                  todos.remove(todo);
+                  return todo.toJson();
+                }
+              }
+              return String.format("Todo with the id \"%s\" not found!", req.params(":id"));
+            }
+    );
+  }
 }
